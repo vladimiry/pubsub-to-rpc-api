ab3dd6b53b920e48bbb1cdf333e22e1af68b7312
Vladimir Y <dev.vladimir.y@gmail.com>
2018-07-17 13:36:43 +0300

    make rxjs peer dependency

--- src/example/readme/client.ts
+++ src/example/readme/client.ts
@@ -6,10 +6,12 @@ const client = SERVICE.caller({
     listener: EM_CLIENT,
 });
 
+// tslint:disable-next-line:no-console
 client("evaluateMathExpression")("32 * 2").subscribe(console.log);
 
 client("httpPing"/*, {timeoutMs: 600}*/)([
     {domain: "https://google.com"},
     {domain: "google.com"},
     {domain: "https://github.com"},
+    // tslint:disable-next-line:no-console
 ]).subscribe(console.log);
